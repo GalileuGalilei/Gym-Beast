f944ab2aea0b1491ea9fd01138d5ddde101211d5
GalileuGalilei <alfredocossetin@outlook.com>
2024-07-12 04:36:14 -0300

    setup project and ragdoll

---
+++ Assets/Character/Scripts/CharacterController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Vector2 walkDirection;
+    [SerializeField] private float walkSpeed = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
